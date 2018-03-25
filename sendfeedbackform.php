a79574b850281b3b648f3f41f3e94292e4656342
justanotherlearner <justanotherlearner@users.noreply.github.com>
2018-03-25 21:18:03 +0530

    feedback form + copyrights

--- sendfeedbackform.php
+++ sendfeedbackform.php
@@ -1,4 +1,28 @@
 <?php
+if (isset($_POST['submit']))  {      
+          // Instructions if $_POST['value'] exist    
+    $name=$_POST["Name"];
+    $Email=$_POST["Email"];
+    $Subject=$_POST["Subject"];
+    $Message=$_POST["Message"];
+
+require 'PHPMailerAutoload.php';
+$mail = new PHPMailer;
+$mail->setFrom('sohampatelforapple@gmail.com', 'shardul');
+$mail->addAddress($Email, 'My Friend');
+$mail->Subject  = "Request to get in touch with Shardul";
+$mail->Body     = "Dear, ".$name.". We have received your request and will be in contact with your shortly. Your message: ".$Message);
+if(!$mail->send()) {
+  echo 'Message was not sent.';
+  echo 'Mailer error: ' . $mail->ErrorInfo;
+} else {
+  echo 'Message has been sent.';
+}
+}
+?>
+
+
+/*
 if (isset($_POST['submit']))    
 {    
           // Instructions if $_POST['value'] exist    
@@ -8,11 +32,11 @@ if (isset($_POST['submit']))
     $Subject=$_POST["Subject"];
     $Message=$_POST["Message"];
     
-//    $to="sohampatel028@gmail.com";
+
     $subject="shardul form";
-//    mail("sohampatel028@gmail.com",$subject,$Message,"From".$name);
+
     $check=mail($Email,"Request to get in touch with Shardul","Dear, ".$name.". We have received your request and will be in contact with your shortly. Your message: ".$Message);
    // header("Location: /web/index.html"); /* Redirect browser */
     //exit();
 }
-?>
+*/
