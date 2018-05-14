350c15f30cc8088faf405117e0820c4509fcc2ca
unknown <sohampatel028@gmail.com>
2018-05-14 18:23:55 +0530

    Revert "Merge branch 'master' of https://github.com/sharduls-site-developers/shardulcollab"
    This reverts commit 089d97be3e2aa4ed30c34cbff6a3762924c8ccec, reversing
    changes made to c68f046ba4f6f6c8f0c2e5bcc029f4d779bb628e.

--- sendfeedbackform.php
+++ sendfeedbackform.php
@@ -1,29 +1,4 @@
-<?php 
-<script type="text/javascript">alert("working")</script>
-if (isset($_POST['submit'])")  {      
-          // Instructions if $_POST['value'] exist    
-    $name=$_POST["Name"];
-    $Email=$_POST["Email"];
-    $Subject=$_POST["Subject"];
-    $Message=$_POST["Message"];
-
-require 'PHPMailerAutoload.php';
-$mail = new PHPMailer;
-$mail->setFrom('sohampatelforapple@gmail.com', 'shardul');
-$mail->addAddress($Email, 'My Friend');
-$mail->Subject  = "Request to get in touch with Shardul";
-$mail->Body     = "Dear, ".$name.". We have received your request and will be in contact with your shortly. Your message: ".$Message);
-if(!$mail->send()) {
-  echo 'Message was not sent.';
-  echo 'Mailer error: ' . $mail->ErrorInfo;
-} else {
-  echo 'Message has been sent.';
-}
-}
-?>
-
-
-/*
+<?php
 if (isset($_POST['submit']))    
 {    
           // Instructions if $_POST['value'] exist    
@@ -33,11 +8,11 @@ if (isset($_POST['submit']))
     $Subject=$_POST["Subject"];
     $Message=$_POST["Message"];
     
-
+//    $to="sohampatel028@gmail.com";
     $subject="shardul form";
-
+//    mail("sohampatel028@gmail.com",$subject,$Message,"From".$name);
     $check=mail($Email,"Request to get in touch with Shardul","Dear, ".$name.". We have received your request and will be in contact with your shortly. Your message: ".$Message);
    // header("Location: /web/index.html"); /* Redirect browser */
     //exit();
 }
-*/
+?>
